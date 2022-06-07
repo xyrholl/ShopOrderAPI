f54ecec0658b59ef7f701e9e9caf06d356feae12
xyrholl <xyrho.v@gmail.com>
2022-06-07 17:56:39 +0900

    [Feat] 결제완료 및 주문 서비스개발
    - 결제성공시 재고수량 감소.
    - 결제성공 후 주문취소시 재고수량 원복.
    - 결제 전 재고수량 감소 없음.
    
    - 다중상품 주문 메서드추가.

--- src/main/java/com/shop/sample/service/OrderService.java
+++ src/main/java/com/shop/sample/service/OrderService.java
@@ -4,12 +4,18 @@ import com.shop.sample.domian.Item;
 import com.shop.sample.domian.Member;
 import com.shop.sample.domian.Order;
 import com.shop.sample.domian.OrderItem;
-import com.shop.sample.exception.ItemNotFoundException;
-import com.shop.sample.exception.MemberNotFoundException;
+import com.shop.sample.dto.OrderDTO;
+import com.shop.sample.dto.OrderItemDTO;
+import com.shop.sample.exception.NotFoundDataException;
 import com.shop.sample.repository.ItemRepository;
 import com.shop.sample.repository.MemberRepository;
 import com.shop.sample.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -21,17 +27,34 @@ public class OrderService {
     private final MemberRepository memberRepository;
     private final ItemRepository itemRepository;
 
-    @Transactional
-    public void order(String memberId, Long itemId, int count){
-        Item findItem = itemRepository.findById(itemId)
-                .orElseThrow(() -> new ItemNotFoundException("상품이 존재하지 않습니다."));
-        Member findMember = memberRepository.findById(memberId)
-                .orElseThrow(() -> new MemberNotFoundException("존재하지않는 회원입니다."));
+    private List<OrderItem> createOrderItems(List<OrderItemDTO> orderItemDtos){
+        List<OrderItem> orderItems = new ArrayList<>();
+        for (OrderItemDTO orderItemDTO : orderItemDtos) {
+            Optional<Item> findItem = itemRepository.findById(orderItemDTO.getItemId());
+            if(findItem.isPresent())orderItems.add(OrderItem.createOrderItem(findItem.get(), orderItemDTO.getCount()));
+        }
+        return orderItems;
+    }
 
-        OrderItem orderItem = OrderItem.createOrderItem(findItem, count);
-        Order order =Order.createOrder(findMember, orderItem);
+    @Transactional
+    public void order_v2(OrderDTO orderDTO){
+        Member findMember = memberRepository.findById(orderDTO.getMemberId())
+            .orElseThrow(() -> new NotFoundDataException("존재하지않는 회원입니다."));
+        List<OrderItem> orderItems = createOrderItems(orderDTO.getItemDTOs());
+        Order order = Order.createOrder(findMember, orderItems.stream().toArray(OrderItem[]::new));
         orderRepository.save(order);
     }
 
+    @Transactional
+    public void order_v1(String memberId, Long itemId, int count){
+        Item findItem = itemRepository.findById(itemId)
+                .orElseThrow(() -> new NotFoundDataException("상품이 존재하지 않습니다."));
+        Member findMember = memberRepository.findById(memberId)
+                .orElseThrow(() -> new NotFoundDataException("존재하지않는 회원입니다."));
+
+        OrderItem orderItem = OrderItem.createOrderItem(findItem, count);
+        Order order = Order.createOrder(findMember, orderItem);
+        orderRepository.save(order);
+    }
     
 }
---
+++ src/main/java/com/shop/sample/service/PaymentService.java
@@ -0,0 +1,26 @@
+package com.shop.sample.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.shop.sample.domian.Order;
+import com.shop.sample.dto.OrderDTO;
+import com.shop.sample.exception.NotFoundDataException;
+import com.shop.sample.repository.OrderRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class PaymentService {
+
+    private final OrderRepository orderRepository;
+
+    @Transactional
+    public void completePymentOrder(OrderDTO orderDTO){
+        Order order = orderRepository.findById(orderDTO.getOrderId())
+            .orElseThrow(() -> new NotFoundDataException());
+        order.completePyment();
+    }
+    
+}
