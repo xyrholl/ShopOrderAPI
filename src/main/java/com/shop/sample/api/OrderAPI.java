1e6343ac8111568af33ce5f77e81e9901d8ef318
xyrholl <xyrho.v@gmail.com>
2022-06-08 17:59:25 +0900

    [Refactor] 와일드카드 변경
    - ResponseEntity 와일드카드 APIMessage로 통일.

--- src/main/java/com/shop/sample/api/OrderAPI.java
+++ src/main/java/com/shop/sample/api/OrderAPI.java
@@ -5,9 +5,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.shop.sample.dto.APIMessage;
+import com.shop.sample.dto.OrderDTO;
+import com.shop.sample.dto.OrderItemDTO;
 import com.shop.sample.dto.Status;
 import com.shop.sample.service.OrderService;
 
@@ -20,7 +23,7 @@ public class OrderAPI {
     private final OrderService orderService;
 
     @GetMapping("/orders")
-    public ResponseEntity<?> info(){
+    public ResponseEntity<APIMessage> info(){
         return ResponseEntity
         .ok()
         .body(APIMessage.builder()
@@ -31,8 +34,12 @@ public class OrderAPI {
     }
 
     @PostMapping("/order")
-    public ResponseEntity<?> order(){
-        return ResponseEntity.ok().body(null);
+    public ResponseEntity<APIMessage> order(@RequestBody OrderItemDTO... orderItemDTOs){
+        return ResponseEntity
+        .ok()
+        .body(
+            null
+        );
     }
 
     @PutMapping("/order/{orderId}")
