8da5dc1d96cc87b508ec6b68e6a6134da4e0ba2a
xyrholl <xyrho.v@gmail.com>
2022-06-22 08:37:54 +0900

    [Feat] 멀티쓰레드 테스트 작성

--- src/test/java/com/shop/sample/application/OrderServiceTest.java
+++ src/test/java/com/shop/sample/application/OrderServiceTest.java
@@ -13,7 +13,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import com.shop.sample.dao.ItemRepository;
 import com.shop.sample.dao.OrderRepository;
-import com.shop.sample.domian.Item;
 import com.shop.sample.domian.Order;
 import com.shop.sample.dto.OrderItemDTO;
 import com.shop.sample.exception.SoldOutException;
@@ -28,55 +27,5 @@ public class OrderServiceTest {
 
     @Autowired ItemRepository itemRepository;
     @Autowired OrderRepository orderRepository;
-
-    private Item findItem(){
-        return itemRepository.findByShopId(1L).get(0);
-    }
-    ExecutorService service = Executors.newFixedThreadPool(10);
-
-    @Test
-    void 주문생성_쓰레드_테스트() throws InterruptedException{
-        OrderItemDTO orderItemDTO = OrderItemDTO.builder().itemId(611019L).count(1).build();
-
-        int count = 100;
-        AtomicInteger successCount = new AtomicInteger();
-        CountDownLatch latch = new CountDownLatch(count);
-        for (int i = 0; i < count; i++) {
-            service.execute(()->{
-                orderService.order(orderItemDTO);
-                successCount.getAndIncrement();
-                latch.countDown();
-            });
-        }
-        latch.await();
-
-        List<Order> orders = orderRepository.findAll();
-        assertThat(orders.size()).isEqualTo(100);
-        assertThat(successCount.get()).isEqualTo(100);
-    }
-
-
-    @Test
-    void 결제완료_멀티쓰레드_SoldOutException() throws InterruptedException{
-        주문생성_쓰레드_테스트();
-        List<Order> orders = orderRepository.findAll();
-
-        int count = 100;
-        AtomicInteger successCount = new AtomicInteger();
-        CountDownLatch latch = new CountDownLatch(count);
-        for (int i = 0; i < count; i++) {
-            Long id = orders.get(i).getId();
-            service.execute(()->{
-                try {
-                    paymentService.completePaymentOrder(id);
-                    successCount.getAndIncrement();
-                    latch.countDown();
-                } catch (SoldOutException e) {
-                    e.printStackTrace();
-                }
-            });
-        }
-        
-    }
 
 }
--- src/test/java/com/shop/sample/application/PaymentServiceTest.java
+++ src/test/java/com/shop/sample/application/PaymentServiceTest.java
@@ -1,21 +1,72 @@
 package com.shop.sample.application;
 
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.shop.sample.dao.OrderRepository;
+import com.shop.sample.domian.Item;
+import com.shop.sample.domian.ItemStatus;
+import com.shop.sample.domian.Order;
+import com.shop.sample.dto.OrderItemDTO;
+import com.shop.sample.exception.SoldOutException;
+
+import static org.assertj.core.api.Assertions.*;
+
 @SpringBootTest
 public class PaymentServiceTest {
 
+    @Autowired OrderRepository orderRepository;
+    @Autowired OrderService orderService;
     @Autowired PaymentService paymentService;
+    @Autowired ItemService itemService;
+
+    private void createOrders(){
+        OrderItemDTO orderItemDTO = OrderItemDTO.builder().itemId(611019L).count(1).build();
+        int count = 100;
+        for (int i = 0; i < count; i++) {
+            orderService.order(orderItemDTO);
+        }
+    }
 
     ExecutorService service = Executors.newFixedThreadPool(10);
 
-    @Test
-    void 결제완료_멀티쓰레드_SoldOutException() {
 
+    @Test
+    void 결제완료_멀티쓰레드_SoldOutException() throws InterruptedException{
+        //given
+        createOrders();
+
+        //when
+        List<Order> orders = orderRepository.findAll();
+        int count = 100;
+        AtomicInteger successCount = new AtomicInteger();
+        CountDownLatch latch = new CountDownLatch(count);
+        for (int i = 0; i < count; i++) {
+            Long id = orders.get(i).getId();
+            service.execute(()->{
+                try {
+                    paymentService.completePaymentOrder(id);
+                    successCount.getAndIncrement();
+                    latch.countDown();
+                } catch (SoldOutException e) {
+                    e.printStackTrace();
+                }
+            });
+        }
+        latch.await(5, TimeUnit.SECONDS);
+        Item item =  itemService.findOne(611019L);
+
+        //then
+        assertThat(successCount.get()).isEqualTo(7);
+        assertThat(item.getStockQuantity()).isEqualTo(0);
+        assertThat(item.getItemStatus()).isEqualTo(ItemStatus.SOLD_OUT);
     }
 }
